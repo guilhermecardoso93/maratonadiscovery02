26f7cfbdffa5235b72f504d7d71e4d5f7dcd29ff
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2022-01-11 21:12:43 -0300

    Aula-2 Fazendo Calculos dos dias

--- src/routes.js
+++ src/routes.js
@@ -11,10 +11,67 @@ const profile = {
   'days-per-week': 5,
   'vacation-per-year': 20
 }
+const jobs = [
+  {
+    name: 'Pizzaria Guloso',
+    'daily-hours': 2,
+    'total-hours': 40,
+    created_at: Date.now()
+  },
+  {
+    name: 'IFood',
+    'daily-hours': 5.5,
+    'total-hours': 100,
+    created_at: Date.now()
+  },
+  {
+    name: 'Site Anime',
+    'daily-hours': 2,
+    'total-hours': 50.5,
+    created_at: Date.now()
+  },
+  {
+    name: 'Portifolio GitHub',
+    'daily-hours': 3.5,
+    'total-hours': 60,
+    created_at: Date.now()
+  }
+]
 
-routes.get('/', (req, res) => res.render(views + 'index'))
+routes.get('/', (req, res) => { 
+  const updatedJobs = jobs.map((job) => {
+
+    const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
+
+    const createdDate = new Date(job.created_at)
+    const dueDay = createdDate.getDate() + Number(remainingDays)
+    const dueDate = createdDate.setDate()
+    return job
+  })
+  
+  
+  
+  return res.render(views + 'index', { jobs })
+
+})
+
 routes.get('/job', (req, res) => res.render(views + 'job'))
+
+routes.post('/job', (req, res) => {
+  const lastId = jobs[jobs.length - 1]?.id || 1
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    'daily-hours': req.body['daily-hours'],
+    'total-hours': req.body['total-hours'],
+    created_at: Date.now()
+  })
+
+  return res.redirect('/')
+})
+
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
+
 routes.get('/profile', (req, res) => res.render(views + 'profile', { profile }))
 routes.get('/index.html', (req, res) => res.redirect('/'))
 
